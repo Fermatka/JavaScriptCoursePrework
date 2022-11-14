c4c55e35737c3d371a3b92776f8d3bc86799ee42
Agata <agatka.fermatka@gmail.com>
2022-11-14 21:10:49 +0100

    id for dot1-3 added in index.html and dot1-3 defined in app.js

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ console.log(arrowLeft);
 
 let arrowRight = document.querySelector(".arrow-right");
 console.log(arrowRight);
+
+let dot1 = document.querySelector("#dot1");
+console.log(dot1);
+
+let dot2 = document.querySelector("#dot2");
+console.log(dot2);
+
+let dot3 = document.querySelector("#dot3");
+console.log(dot3);
